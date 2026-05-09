44cacfb4d2c9f47ea37d37891aaa828db321a873
Fernando Eufrásio <fernando.epm1@gmail.com>
2026-05-09 15:17:11 -0300

    feat: run the cpu in a loop on emulator start

--- lib/akane/emulator.rb
+++ lib/akane/emulator.rb
@@ -6,16 +6,37 @@ module Akane
     def self.start(file_path)
       puts "Emulator started with #{file_path}"
       cartridge = Cartridge.load_rom(file_path)
-      ppu = Gameboy::Ppu.new
-      wram = Gameboy::Ram.new(8 * 1_024)
+      wram = Gameboy::Ram.new(8192)
       hram = Gameboy::Ram.new(127)
       interrupts = Gameboy::Interrupts.new
+
+      ppu = Gameboy::Ppu.new(interrupts)
       timer = Gameboy::Timer.new(interrupts)
       serial = Gameboy::Serial.new(interrupts)
       joypad = Gameboy::Joypad.new(interrupts)
-      Gameboy::Bus.new(
-        cartridge, ppu, wram, hram, interrupts, timer, serial, joypad
+
+      bus = Gameboy::Bus.new(
+        cartridge: cartridge,
+        ppu: ppu,
+        wram: wram,
+        hram: hram,
+        interrupts: interrupts,
+        timer: timer,
+        serial: serial,
+        joypad: joypad
       )
+
+      advance_components = proc do |cycles|
+        puts "All components advanced by #{cycles} t-cycles"
+      end
+
+      cpu = Gameboy::Cpu.new(bus, interrupts, advance_components)
+
+      i = 0
+      while i < 10
+        cpu.run
+        i += 1
+      end
     end
   end
 end
