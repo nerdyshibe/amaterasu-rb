516cbf8ce64bb21df48c5c2d85b561deec895153
Fernando Eufrásio <fernando.epm1@gmail.com>
2026-05-08 22:05:50 -0300

    feat: add main emulator class and initial ppu

--- lib/akane/emulator.rb
+++ lib/akane/emulator.rb
@@ -5,7 +5,17 @@ module Akane
   class Emulator
     def self.start(file_path)
       puts "Emulator started with #{file_path}"
-      Akane::Cartridge.load_rom(file_path)
+      cartridge = Cartridge.load_rom(file_path)
+      ppu = Gameboy::Ppu.new
+      wram = Gameboy::Ram.new(8 * 1_024)
+      hram = Gameboy::Ram.new(127)
+      interrupts = Gameboy::Interrupts.new
+      timer = Gameboy::Timer.new(interrupts)
+      serial = Gameboy::Serial.new(interrupts)
+      joypad = Gameboy::Joypad.new(interrupts)
+      Gameboy::Bus.new(
+        cartridge, ppu, wram, hram, interrupts, timer, serial, joypad
+      )
     end
   end
 end
---
+++ lib/akane/gameboy/ppu.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+module Akane
+  module Gameboy
+    # Models the PPU behavior from the Game Boy.
+    class Ppu
+      def initialize
+        @vram = Ram.new(8_192)
+        @oam = Ram.new(160)
+      end
+
+      # Returns a 8-bit value stored in VRAM in a given offset.
+      def read_vram(offset)
+        @vram.read_byte(offset)
+      end
+
+      # Stores a 8-bit value in VRAM in a given offset.
+      def write_vram(offset, value)
+        @vram.write_byte(offset, value)
+      end
+
+      # Returns a 8-bit value stored in OAM in a given offset.
+      def read_oam(offset)
+        @oam.read_byte(offset)
+      end
+
+      # Stores a 8-bit value in OAM in a given offset.
+      def write_oam(offset, value)
+        @oam.write_byte(offset, value)
+      end
+    end
+  end
+end
