4634c106854503a8f1e161569e7fd01fc3b2515e
Fernando Eufrásio <fernando.epm1@gmail.com>
2026-05-14 11:16:03 -0300

    feat: add new base instruction class with shared behavior

---
+++ lib/akane/gameboy/cpu/instructions/base.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+module Akane
+  module Gameboy
+    class Cpu
+      module Instructions
+        # Defines the mnemonic, bytes and logic for the NOP instruction.
+        class Base
+          attr_reader :mnemonic, :bytes, :m_cycles
+
+          def initialize(cpu:)
+            @cpu = cpu
+            @registers = cpu.registers
+          end
+
+          def execute
+            @logic.call
+          end
+
+          private
+
+          def format_operand(operand)
+            return 'n8'   if operand == :imm8
+            return 'n16'  if operand == :imm16
+            return '[HL]' if operand == :mem_hl
+
+            operand.upcase
+          end
+
+          def fetch_cost(operand)
+            return 1 if operand == :imm8
+            return 2 if operand == :imm16
+
+            0
+          end
+
+          def memory_cost(operand)
+            return 1 if operand == :imm8
+            return 2 if operand == :imm16
+            return 1 if operand == :mem_hl
+
+            0
+          end
+        end
+      end
+    end
+  end
+end
