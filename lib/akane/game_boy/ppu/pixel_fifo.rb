4a8002019c167d82febf6ccbbb542f8737ab30ad
Fernando Eufrásio <fernando.epm1@gmail.com>
2026-06-02 09:00:56 -0300

    refactor: fix memory allocation issue in the pixel fifo

--- lib/akane/game_boy/ppu/pixel_fifo.rb
+++ lib/akane/game_boy/ppu/pixel_fifo.rb
@@ -7,32 +7,40 @@ module Akane
       class PixelFifo
         MAX_PIXELS = 16
 
+        # Array of raw color indices (0 - 3).
         attr_reader :pixels
 
         def initialize
-          @pixels = Array.new
+          @pixel_buffer = Array.new
         end
 
+        # The Pixel Fetcher is the pixel producer,
+        # it produces 8 color indices (1 for each pixel) and attempts
+        # to push all 8 at the same time until it succeeds.
+        #
+        # It will only succeed if the FIFO is empty.
+        #
         # @return [Boolean] If the push was successful or not.
-        def push?(pixels)
-          return false unless @pixels.empty?
+        def push?(pushed_pixels)
+          return false unless @pixel_buffer.empty?
 
-          pixels.each_with_index do |pixel, index|
-            @pixels[index] = pixel
-          end
+          pushed_pixels.each { |pp| @pixel_buffer << pp }
 
           true
         end
 
+        # To follow the FIFO rules, elements need to be popped
+        # from left to right, that is why we need to use Array#shift.
+        #
         # @return [Integer] 2 bit number representing the color id of the pixel.
         def pop_pixel
-          return if @pixels.empty?
+          return if @pixel_buffer.empty?
 
-          @pixels.shift
+          @pixel_buffer.shift
         end
 
         def clear
-          @pixels.clear
+          @pixel_buffer.clear
         end
       end
     end
