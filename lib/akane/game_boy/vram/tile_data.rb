71d297afd0b0294bec6cd5658ab419b814a07364
Fernando Eufrásio <fernando.epm1@gmail.com>
2026-06-03 20:47:41 -0300

    feat: create a better model for the tile data and tile

--- lib/akane/game_boy/vram/tile_data.rb
+++ lib/akane/game_boy/vram/tile_data.rb
@@ -6,15 +6,18 @@ module Akane
       # Models the Tile Data that lives in the VRAM.
       class TileData
         TILE_SIZE = 16
+        TILE_ENTRIES = 384
+
+        attr_reader :tiles, :addressing_mode, :base_offset
 
         # @param vram_data [Array] Original VRAM @data array object.
         def initialize(vram_data:)
-          @vram_data = vram_data
-
           @addressing_mode = :unsigned
           @base_offset     = 0x0000
-          @tile_row        = Hash.new
-          @tile_row_pixels = Array.new
+
+          @tiles = Array.new(TILE_ENTRIES) do |index|
+            Tile.new(vram_data: vram_data, tile_index: index)
+          end
         end
 
         # @param mode [Symbol] Either :unsigned or :signed.
@@ -28,54 +31,20 @@ module Akane
         # the tile.
         #
         # @param tile_index [Integer] 8-bit value representing the tile index.
-        # @param current_line [Integer] Current line being scanned in Drawing mode.
-        # @return [Hash] 2 bytes from the given Tile index at that current line.
-        def tile_row(tile_index, current_line)
+        # @return [Vram::Tile]
+        def tile_at(tile_index)
           tile_index = sign_value(tile_index) if @addressing_mode == :signed
-          current_line &= 0b111
+          tile_offset = @base_offset / Tile::SIZE_IN_BYTES
 
-          tile_offset     = tile_index * TILE_SIZE
-          tile_row_offset = current_line * 2
-
-          final_offset = @base_offset + tile_offset + tile_row_offset
-
-          @tile_row[:low_byte]  = @vram_data[final_offset]
-          @tile_row[:high_byte] = @vram_data[final_offset + 1]
-          @tile_row[:pixels]    = decode_pixels
-
-          @tile_row
+          @tiles[tile_offset + tile_index]
         end
 
         private
 
-        # @param index [Integer] The current Tile index.
+        # @param index [Integer] The current Tile index (8-bit value).
         # @return [Integer] A value between -128 and +127.
         def sign_value(index)
           index >= 128 ? index - 256 : index
-        end
-
-        def decode_pixels
-          @tile_row_pixels.clear unless @tile_row_pixels.empty?
-          bit = 7
-
-          while bit >= 0
-            low_bit  = @tile_row[:low_byte][bit]
-            high_bit = @tile_row[:high_byte][bit]
-
-            color_id = (high_bit << 1) | low_bit
-            @tile_row_pixels << color_id
-
-            bit -= 1
-          end
-
-          @tile_row_pixels
-        end
-
-        def inspect
-          "#<Vram::TileData \n" \
-            "@block0=#{@block0_tiles}\n" \
-            "@block1=#{@block1_tiles}\n" \
-            "@block2=#{@block2_tiles}>"
         end
       end
     end
